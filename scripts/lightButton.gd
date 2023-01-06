c4ec46563c4a5e3bc4b8bb2b33b31ed98b41d5f9
giulianubile <giulianubile98@gmail.com>
2023-01-06 16:57:24 +0100

    changes

--- scripts/lightButton.gd
+++ scripts/lightButton.gd
@@ -3,26 +3,25 @@ extends Area
 var on = false
 
 func _on_lightButton_body_entered(body):
-	if body.name == "greenFish" or body.name == "PezDani":
-			body.movement.z = -1*body.speed
-			body.rotation_degrees = Vector3(0,180,0)
-			if on == false:
-				$AnimationPlayer.play("on")
-				on = true
-				for child in get_children():
-					if child.name == "christmasTree":
-						var tree = get_node("christmasTree")						
-						var lights = tree.get_node("lights")
-						var animation = lights.get_node("AnimationPlayer")
-						animation.play("on")
-						animation.get_animation("on").set_loop(true)
-
-			elif on == true:
-				$AnimationPlayer.play("off")
-				on = false
-				for child in get_children():
-					if child.name == "christmasTree":
-						var tree = get_node("christmasTree")
-						var lights = tree.get_node("lights")
-						var animation = lights.get_node("AnimationPlayer")
-						animation.play("off")
+	if body.name == "GreenFish" or body.name == "PezDani":
+		body.movement.z = -1*body.speed * body.movement.z
+		body.rotation_degrees += -1*body.rotation_degrees
+		if on == false:
+			$AnimationPlayer.play("on")
+			on = true
+			for child in get_children():
+				if child.name == "ChristmasTree":
+					var tree = get_node("ChristmasTree")						
+					var lights = tree.get_node("lights")
+					var animation = lights.get_node("AnimationPlayer")
+					animation.play("on")
+					animation.get_animation("on").set_loop(true)
+		elif on == true:
+			$AnimationPlayer.play("off")
+			on = false
+			for child in get_children():
+				if child.name == "ChristmasTree":
+					var tree = get_node("ChristmasTree")
+					var lights = tree.get_node("lights")
+					var animation = lights.get_node("AnimationPlayer")
+					animation.play("off")
--- scripts/snowButton.gd
+++ scripts/snowButton.gd
@@ -4,22 +4,22 @@ var on = false
 
 
 func _on_snowButton_body_entered(body):
-	if body.name == "greenFish" or body.name == "PezDani":
-		body.movement.z = -1*body.speed
-		body.rotation_degrees = Vector3(0,180,0)
+	if body.name == "GreenFish" or body.name == "PezDani":
+		body.movement.z = -1*body.speed * body.movement.z
+		body.rotation_degrees += -1*body.rotation_degrees
 		
 		if on == false:
 			$AnimationPlayer.play("on")
 			on = true
 			for child in get_children():
-				if child.name == "snow":
+				if child.name == "Snow":
 					child.emitting = true
 		
 		elif on == true:
 			$AnimationPlayer.play("off")
 			on = false
 			for child in get_children():
-				if child.name == "snow":
+				if child.name == "Snow":
 					child.emitting = false
 				
 			
