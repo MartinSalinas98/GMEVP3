b2b45da20e9c932fae3229589d4649e5d7e5757a
giulianubile <giulianubile98@gmail.com>
2023-01-08 15:23:15 +0100

    eating

--- scripts/PlayerFish.gd
+++ scripts/PlayerFish.gd
@@ -3,10 +3,14 @@ extends KinematicBody
 var speed = 1
 var movement = Vector3(0,0,0)
 var camera_on = false
+var eating = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("Still")
+	$AnimationPlayer.get_animation("Swimming").set_loop(true)
+	$AnimationPlayer.get_animation("Eating").set_loop(true)
+	$AnimationPlayer.get_animation("Eating").set_loop(true)
 	
 
 
@@ -14,33 +18,55 @@ func _physics_process(delta):
 	movement = movement.normalized()
 	translation += movement * speed * delta
 	
-	if movement == Vector3(0,0,0):
-		$AnimationPlayer.play("Still")
-	else:
-		$AnimationPlayer.play("Swimming")
+	
+	
+	
 	
 
 func _input(event):
+	
 	if event.is_action_pressed("ui_down"):
+		$AnimationPlayer.play("Swimming")
 		movement = Vector3(1,0,0)
 		rotation_degrees = Vector3(0,0,0)
 	elif event.is_action_pressed("ui_up"):
+		$AnimationPlayer.play("Swimming")
 		movement = Vector3(-1,0,0)
 		rotation_degrees = Vector3(0,180,0)
 	elif event.is_action_pressed("ui_left"):
+		$AnimationPlayer.play("Swimming")
 		movement = Vector3(0,0,1)
 		rotation_degrees = Vector3(0,-90,0)
 	elif event.is_action_pressed("ui_right"):
+		$AnimationPlayer.play("Swimming")
 		movement = Vector3(0,0,-1)
 		rotation_degrees = Vector3(0,90,0)
 	elif event.is_action_pressed("ui_su"):
+		$AnimationPlayer.play("Swimming")
 		movement = Vector3(0,1,0)
 		rotation_degrees = Vector3(0,-180,90)
 	elif event.is_action_pressed("ui_giu"):
+		$AnimationPlayer.play("Swimming")
 		movement = Vector3(0,-1,0)
 		rotation_degrees = Vector3(0,0,-90)
 	elif event.is_action_pressed("ui_accept"):
+		$AnimationPlayer.play("Still")
 		movement = Vector3(0,0,0)
+		
+		
+	elif event.is_action_pressed("ui_eating"):
+		if eating == false:
+			$AnimationPlayer.play("Eating")
+			eating =true
+			
+		elif eating == true:
+			$AnimationPlayer.play("Swimming")
+			eating = false
+		
+	
+		
+		
+	
 		
 	if event.is_action_pressed("ui_cam"):
 		if camera_on == false:
